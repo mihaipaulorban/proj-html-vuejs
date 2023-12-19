7432207da17f94397ef8789a283696362742f1e2
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-19 14:23:15 +0100

    feat: scaffolding e impostazione file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script>
+// Importazione componenti principali
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
   name: 'DefaultTemplate',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
 
 <style scoped lang="scss">
 @use './assets/scss/partials/variables' as *;
-@use './assets/scss/partials/common ' as *;
+@use './assets/scss/partials/common' as *;
 @use './assets/scss/partials/reset' as *;
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'AppMain',
+};
+</script>
+
+<template>
+  <h2>Main</h2>
+</template>
+
+<style scoped lang="scss"></style>
