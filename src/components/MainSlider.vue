b0215a6abd2ee00690b63b469596848e646e93a1
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-21 12:08:52 +0100

    feat: terzo slider funzionante

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -1,6 +1,24 @@
 <script>
+import { sliderInfo } from '../store';
+
 export default {
   name: 'MainSlider',
+  data() {
+    return {
+      currentIndex: 0,
+      sliderInfo: sliderInfo,
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.sliderInfo.length;
+    },
+    prevSlide() {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.sliderInfo.length) %
+        this.sliderInfo.length;
+    },
+  },
 };
 </script>
 
@@ -8,70 +26,60 @@ export default {
   <div id="carouselExampleCaptions" class="carousel slide">
     <div class="carousel-indicators">
       <button
+        v-for="(slide, index) in sliderInfo"
+        :key="'indicator-' + index"
         type="button"
         data-bs-target="#carouselExampleCaptions"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="Slide 1"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide-to="1"
-        aria-label="Slide 2"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide-to="2"
-        aria-label="Slide 3"
+        :data-bs-slide-to="index"
+        :class="{ active: index === currentIndex }"
+        :aria-label="'Slide ' + (index + 1)"
+        @click="currentIndex = index"
       ></button>
     </div>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img
-          src="../../public/img/gallery_07-690x506.jpg"
-          class="d-block w-100"
-          alt="..."
-        />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+      <div
+        v-for="(slide, index) in sliderInfo"
+        :key="'slide-' + index"
+        :class="['carousel-item', { active: index === currentIndex }]"
+      >
+        <div class="slide-container">
+          <div class="info-container">
+            <div
+              v-for="(info, infoIndex) in slide"
+              :key="'info-' + infoIndex"
+              class="info"
+            >
+              <div class="icon">
+                <h2>{{ info.icon }}</h2>
+              </div>
+              <div class="title">
+                <h3>{{ info.name }}</h3>
+              </div>
+              <div class="text">
+                <p>{{ info.text }}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <button
       class="carousel-control-prev"
+      @click="prevSlide"
       type="button"
       data-bs-target="#carouselExampleCaptions"
       data-bs-slide="prev"
     >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
     </button>
     <button
       class="carousel-control-next"
+      @click="nextSlide"
       type="button"
       data-bs-target="#carouselExampleCaptions"
       data-bs-slide="next"
     >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
     </button>
   </div>
 </template>
@@ -79,6 +87,63 @@ export default {
 <style scoped lang="scss">
 @use '../assets/scss/partials/variables' as *;
 @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+.carousel-inner {
+  background-image: url(../../public/img/parallax_01.jpg);
+  background-position: 0;
+}
+.slide-container {
+  width: 100%;
+  padding: 150px 0;
+  display: flex;
+  justify-content: center;
+  .info-container {
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 150px;
+        height: 150px;
+        padding: 50px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 1rem;
+        border: 1px $color-tertiary solid;
+
+        h2 {
+          margin: 0;
+          font-size: 40px;
+          color: $color-tertiary;
+          font-weight: 200;
+        }
+      }
+
+      .title {
+        text-align: center;
+        color: $color-tertiary;
+        h3 {
+          font-weight: 300;
+          font-size: 25px;
+          text-wrap: wrap;
+        }
+      }
+
+      .text {
+        text-align: center;
+        color: $color-tertiary;
+        font-weight: 300;
+        font-size: 15px;
+        text-wrap: wrap;
+      }
+    }
+  }
+}
 
 // Stili Testo slide
 .carousel-caption {
--- src/store.js
+++ src/store.js
@@ -88,3 +88,137 @@ export const carouselImg = reactive([
     alt: 'Immagine 3',
   },
 ]);
+export const sliderInfo = [
+  [
+    {
+      icon: '312',
+      name: 'Eager Explorers',
+      text: 'Embracing curiosity within every learning activity.',
+    },
+    {
+      icon: '127',
+      name: 'Artistic Minds',
+      text: 'Creative expression through various artistic techniques.',
+    },
+    {
+      icon: '82%',
+      name: 'Involved Families',
+      text: 'Strong partnerships for a supportive educational journey.',
+    },
+    {
+      icon: '634',
+      name: 'Healthy Snacks',
+      text: 'Wholesome treats to fuel active young minds.',
+    },
+  ],
+  [
+    {
+      icon: '401',
+      name: 'Tech Whizzes',
+      text: 'Integrating technology with fun educational apps.',
+    },
+    {
+      icon: '169',
+      name: 'Bookworms',
+      text: 'Instilling a love for reading from early stages.',
+    },
+    {
+      icon: '95%',
+      name: 'Smiling Faces',
+      text: 'A smile a day keeps the learning blues away.',
+    },
+    {
+      icon: '890',
+      name: 'Playful Spaces',
+      text: 'Safe environments that inspire imaginative play.',
+    },
+  ],
+  [
+    {
+      icon: '524',
+      name: 'Little Athletes',
+      text: 'Promoting teamwork and physical fitness for all.',
+    },
+    {
+      icon: '268',
+      name: 'Science Lab',
+      text: 'Discovering the wonders of science hands-on.',
+    },
+    {
+      icon: '78%',
+      name: 'Community Spirit',
+      text: 'Learning to give back through local initiatives.',
+    },
+    {
+      icon: '701',
+      name: 'Field Trips',
+      text: 'Educational excursions that expand horizons.',
+    },
+  ],
+  [
+    {
+      icon: '359',
+      name: 'Nature Lovers',
+      text: 'Exploring the environment with respect and wonder.',
+    },
+    {
+      icon: '212',
+      name: 'Music Makers',
+      text: 'Harmony and melody in daily rhythms.',
+    },
+    {
+      icon: '89%',
+      name: 'Confident Speakers',
+      text: 'Nurturing orators with storytelling and drama.',
+    },
+    {
+      icon: '953',
+      name: 'Cultural Festivals',
+      text: 'Celebrating diversity through global traditions.',
+    },
+  ],
+  [
+    {
+      icon: '488',
+      name: 'Budding Artists',
+      text: 'Every color and stroke is a step towards creativity.',
+    },
+    {
+      icon: '194',
+      name: 'Language Lessons',
+      text: 'Bilingual beginnings for a world of opportunities.',
+    },
+    {
+      icon: '67%',
+      name: 'Parent Workshops',
+      text: 'Collaborative learning extends to the whole family.',
+    },
+    {
+      icon: '869',
+      name: 'Garden Club',
+      text: 'Growing minds and plants side by side.',
+    },
+  ],
+  [
+    {
+      icon: '536',
+      name: 'Future Leaders',
+      text: 'Fostering decision-making and leadership skills early on.',
+    },
+    {
+      icon: '273',
+      name: 'Math Magicians',
+      text: 'The magic of numbers revealed through fun activities.',
+    },
+    {
+      icon: '91%',
+      name: 'Supportive Environment',
+      text: 'Every child is supported to reach their potential.',
+    },
+    {
+      icon: '1078',
+      name: 'Nutrition Education',
+      text: 'Learning about healthy eating by doing and tasting.',
+    },
+  ],
+];
